3a4c8969746106df96a80ff877e2ad306c2dc926
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-06 21:49:05 +0530

    filter working added

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,43 +1,57 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Filter from "./Filter";
+import Search from "./Search";
+import Table from "./Table";
 
-class App extends React.Component{
-     
-   state = {
-     movies:[],
-     genre:[],
-   };
+class App extends React.Component {
+	state = {
+		movies: [],
+		genre: [],
+		selectedFilter: "All Genre",
+	};
 
-  componentDidMount(){
-    let f = async ()=>{
-      let responseMovies = await fetch("/movies");
-      let responseGenre = await fetch("/genre");
-      // ye dono, ek object jiske andhar hamara data aa raha hain server se via API
-      let moviesJson = await responseMovies.json();
-      let genreJson = await responseGenre.json();
-     
-      this.setState({
-        movies:moviesJson,
-        genre:genreJson,
-      });
-    };
-     f();
+	setFilter = (filter) => {
+		this.setState({ selectedFilter: filter });
+	};
 
-  }
+	componentDidMount() {
+		let f = async () => {
+			let responseMovies = await fetch("/movies");
+			let responseGenre = await fetch("/genre");
+			// ye dono, ek object jiske andhar hamara data aa raha hain server se via API
+			let moviesJson = await responseMovies.json();
+			let genreJson = await responseGenre.json();
 
-  render(){
-    return (
-      <div>
-        <Navbar/>
+			this.setState({
+				movies: moviesJson,
+				genre: genreJson,
+			});
+		};
+		f();
+	}
 
-        <div className="row">
+	render() {
+		return (
+			<div>
+				<Navbar />
 
-          <Filter genreData = {this.state.genre}/>
-          
-        </div>
-      </div>
-    )
-  }
+				<div className="row">
+					<Filter
+						handleFilter={this.setFilter}
+						selectedFilter={this.state.selectedFilter}
+						genreData={this.state.genre}
+					/>
+					<div class="col-9 p-4">
+						<Search />
+						<Table
+							movieData={this.state.movies}
+							selectedFilter={this.state.selectedFilter}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
--- movies/src/Filter.jsx
+++ movies/src/Filter.jsx
@@ -1,17 +1,36 @@
-
-let Filter = (props)=>{
-    console.log(props.genreData);
-    return (
-    <div class="col-3">
-        <ul class="list-group m-4">
-            <li class="list-group-item">All Genre</li>
-            {
-                props.genreData.map((el)=>{
-                        return <li key = {el._id} class="list-group-item">{el.name}</li>
-                })
-            }
-        </ul>
-    </div>);
-}
+let Filter = (props) => {
+	return (
+		<div class="col-3">
+			<ul class="list-group m-4">
+				<li
+					class="list-group-item"
+					onClick={(e) => {
+						props.handleFilter("All Genre");
+					}}
+					className={`list-group-item ${
+						props.selectedFilter == "All Genre" ? "active" : ""
+					}`}
+				>
+					All Genre
+				</li>
+				{props.genreData.map((el) => {
+					return (
+						<li
+							key={el._id}
+							onClick={(e) => {
+								props.handleFilter(el.name);
+							}}
+							className={`list-group-item ${
+								props.selectedFilter == el.name ? "active" : ""
+							}`}
+						>
+							{el.name}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
 
 export default Filter;
